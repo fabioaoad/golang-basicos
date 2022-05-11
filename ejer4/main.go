2745534b126faf10f521751f7092190c7c58df1b
LAPTOP-63JVQLDS\Propietario <fabioaoad@gmail.com>
2022-05-11 20:14:55 -0300

    ejer 4 - parte 2

--- ejer4/main.go
+++ ejer4/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 var numero1 int
 var numero2 int
+var resultado int
+var leyenda string
 
 func main() {
 	fmt.Println("Ingrese numero 1: ")
@@ -13,5 +17,14 @@ func main() {
 
 	fmt.Println("Ingrese numero 2: ")
 	fmt.Scanln(&numero2)
-	fmt.Printf("%d", numero1+numero2)
+
+	fmt.Println("Descripcion: ")
+
+	scaner := bufio.NewScanner(os.Stdin) // escanea por teclado, es deir estandar input
+	if scaner.Scan() {
+		leyenda = scaner.Text()
+	}
+
+	resultado = numero1 + numero2
+	fmt.Println(leyenda, resultado)
 }
