fc09ed72bf9ae14379f6c02041188b4a350b087e
LAPTOP-63JVQLDS\Propietario <fabioaoad@gmail.com>
2022-05-14 20:17:15 -0300

    interfaces en go - parte 2

--- ejer11/main.go
+++ ejer11/main.go
@@ -30,27 +30,25 @@ type hombre struct {
 	respirando bool
 	pensando bool
 	comiendo bool
+	esHombre bool
 }
 
 type mujer struct {
-	edad int
-	altura float32
-	peso float32
-	respirando bool
-	pensando bool
-	comiendo bool
+	hombre
 }
 
 func (h *hombre) respirar() { h.respirando = true}
 func (h *hombre) comer() { h.comiendo = true}
 func (h *hombre) pensar() { h.pensando = true}
-func (h *hombre) sexo() string { return "Hombre"}
+func (h *hombre) sexo() string {
+	if h.esHombre == true {
+		return "Hombre"
+	} else {
+		return "Mujer"
+	}
+}
 
 
-func (m *mujer) respirar() { m.respirando = true}
-func (m *mujer) comer() { m.comiendo = true}
-func (m *mujer) pensar() { m.pensando = true}
-func (m *mujer) sexo() string { return "Mujer"}
 
 func HumanoRespirando(hu humano) {
 	hu.respirar()
@@ -60,6 +58,7 @@ func HumanoRespirando(hu humano) {
 func main()  {
 
 Pedro := new(hombre)
+Pedro.esHombre = true
 HumanoRespirando(Pedro)
 
 Maria := new(mujer)
