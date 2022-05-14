25a497783f169413a2b7f7fba9722f6bd615bf1e
LAPTOP-63JVQLDS\Propietario <fabioaoad@gmail.com>
2022-05-14 19:43:37 -0300

    go - objetos

---
+++ ejer10/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	us "goland-basicos/ejer10/user"
+	"time"
+)
+
+type usuario struct {
+	Id        int
+	Nombre    string
+	FechaAlta time.Time
+	Status    bool
+}
+
+type martin struct {
+	us.Usuario
+}
+
+func main() {
+	User := new(usuario)
+	User.Id = 10
+	User.Nombre = "Fabio"
+	fmt.Println(User)
+
+	fmt.Println("--------------------")
+
+	u := new(martin)
+	u.AltaUsuario(1, "Fabio Aoad", time.Now(), true)
+	fmt.Println(u)
+
+}
