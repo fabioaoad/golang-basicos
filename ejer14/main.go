d54066a9383c89e0b7479d0fba720fadf836ee50
LAPTOP-63JVQLDS\Propietario <fabioaoad@gmail.com>
2022-05-15 10:06:30 -0300

    defer - panic - recover

---
+++ ejer14/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+
+)
+
+func main()  {
+	/* archivo:= "prueba.txt"
+	f, err := os.Open(archivo)
+	defer f.Close()
+	if err != nil{
+		fmt.Println("Error al abriendo el archivo")
+		os.Exit(1)
+	} */
+
+
+	ejemploPanic()
+}
+func ejemploPanic()  {
+	defer func() {
+		reco := recover()
+		if reco != nil {
+			log.Fatalf("Ocurrio un error que generó un Panic \n %v", reco)
+		}
+	}()
+	a := 1
+	if a==1 {
+		panic("se encontro el valor de 1")
+	}
+}
